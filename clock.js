0b6276ca8314f7cad217cd351c4d3dc26a464f22
00-au <dmdgi37@naver.com>
2021-02-09 00:39:11 +0900

    clock-02
    made the clock work / time is 0 for less than 10

--- clock.js
+++ clock.js
@@ -6,11 +6,15 @@ function getTime(){
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}`: seconds}`;
+    //삼항연산자를 이용해 10보다 작을 때 0을 추가한다. (조건을 적는 것임) seconds < 10 ? `0${seconds}`: seconds}
 }
 
 function init(){
     getTime();
+    setInterval(getTime, 1000);
 }
+
+//setInterval(함수, 초단위) 로 함수를 흘러가게 만들어 줄 수 있음
 
 init();
